0bf52b3413a65ac77d855e3ce6e59a8d41c19e51
zameddy <eddy.zam@gmail.com>
2015-02-18 15:10:31 +0100

    second commit

---
+++ monappli-domaine/src/main/java/com/objis/demomaven/App.java
@@ -0,0 +1,13 @@
+package com.objis.demomaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
